1d5b36bf7cccbcf41ef2c3aff030c83a9f043a13
emrecnulkr <emrecnulkr@gmail.com>
2017-07-11 16:47:01 +0300

    Arastirmalar

---
+++ 3D Lazer Tarayıcı Nasıl Yapılır_ _ Türk İşi_files/custom.css.php
@@ -0,0 +1,26 @@
+html, body, h1, h2, h3, h4, h5, h6, p, span{
+	font-family: 'PT Sans Caption', sans-serif;
+}
+h1{font-size:30px; }
+h2{font-size:22px; }
+h3{font-size:17.5px; }
+body{font-size: 13px; }
+.main-menu .nav > li > a {; }
+#head-carousel .is-carousel {
+    height: 360px;
+}
+#head-carousel .video-item {
+	width: 640px;
+    height: 360px;
+}
+#head-carousel #metro-carousel .video-item .item-thumbnail img {
+    width: 640px;
+}
+#head-carousel #metro-carousel .video-item > .video-item {
+    width: 320px;
+    height: 180px;
+}
+#head-carousel #metro-carousel.is-carousel .carousel-button a, #head-carousel #big-carousel.is-carousel .carousel-button a {
+    height: 360px;
+    line-height: 360px;
+}
